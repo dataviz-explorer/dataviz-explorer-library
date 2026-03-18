ed7ed0ecdc965c5de1115e3909fdc40aad65c8d7
dataviz-explorer <noreenlewis48@gmail.com>
2026-03-18 16:28:13 -0400

    Update branch.sql

--- tables/branch.sql
+++ tables/branch.sql
@@ -1,4 +1,11 @@
- CREATE TABLE dataviz_library.branch (
+ -- ================================================
+-- DataViz Explorer Library Database
+-- Created by: Noreen Lewis | DataViz Explorer®
+-- Table: branch
+-- Description: Stores library branch locations
+-- ================================================
+
+CREATE TABLE dataviz_library.branch (
     id        SERIAL,
     name      VARCHAR(128) UNIQUE,
     location  VARCHAR(256),
