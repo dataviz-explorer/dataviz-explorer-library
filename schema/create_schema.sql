15dc44683293e53ae3a61d14a019cbd709fdcb55
dataviz-explorer <noreenlewis48@gmail.com>
2026-03-18 16:07:48 -0400

    Create create_schema.sql

---
+++ schema/create_schema.sql
@@ -0,0 +1,9 @@
+-- ================================================
+-- DataViz Explorer Library Database
+-- Created by: Noreen Lewis | DataViz Explorer®
+-- Date: March 2026
+-- Description: Creates the dataviz_library schema
+-- ================================================
+
+CREATE SCHEMA dataviz_library;
+SET search_path TO dataviz_library;
