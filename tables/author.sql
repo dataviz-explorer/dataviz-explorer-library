61884f158b90dbd8b7639bc35e70b34f99b38463
dataviz-explorer <noreenlewis48@gmail.com>
2026-03-18 16:24:12 -0400

    Update author.sql

--- tables/author.sql
+++ tables/author.sql
@@ -1,6 +1,14 @@
+ -- ================================================
+-- DataViz Explorer Library Database
+-- Created by: Noreen Lewis | DataViz Explorer®
+-- Table: author
+-- Description: Stores information about book authors
+-- ================================================
+
 CREATE TABLE dataviz_library.author (
     id           SERIAL,
     first_name   VARCHAR(128),
     last_name    VARCHAR(128),
     nationality  VARCHAR(128),
     PRIMARY KEY(id)
+);
