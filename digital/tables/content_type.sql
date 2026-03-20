b7331574ad1afcd48000a0ddf831395f9051ff32
DataViz Explorer® | Noreen Lewis <noreenlewis48@gmail.com>
2026-03-20 17:58:39 -0400

    Update content_type.sql

--- digital/tables/content_type.sql
+++ digital/tables/content_type.sql
@@ -1,1 +1,13 @@
+-- ================================================
+-- DataViz Explorer Digital Library Database
+-- Created by: Noreen Lewis | DataViz Explorer®
+-- Table: content_type
+-- Description: Stores digital content categories
+-- ================================================
 
+CREATE TABLE dataviz_library.content_type (
+    id          SERIAL,
+    name        VARCHAR(128) UNIQUE,
+    description VARCHAR(256),
+    PRIMARY KEY(id)
+);
